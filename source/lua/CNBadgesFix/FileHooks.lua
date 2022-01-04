eec961dfefc2d3a0a1e098f73abcad6e500b5bd6
striter <letsbattle@foxmail.com>
2022-01-05 01:01:04 +0800

    Optimize

--- source/lua/CNBadgesFix/FileHooks.lua
+++ source/lua/CNBadgesFix/FileHooks.lua
@@ -1,8 +1,1 @@
-if Client then
-  ModLoader.SetupFileHook("lua/Badges_Client.lua", "lua/CNBadgesFix/Badges_Client.lua", "replace")
-  ModLoader.SetupFileHook("lua/GUIUnitStatus.lua", "lua/CNBadgesFix/GUIUnitStatus.lua", "replace")
-end
-
-if Server then
-  ModLoader.SetupFileHook("lua/Badges_Server.lua", "lua/CNBadgesFix/Badges_Server.lua", "replace")
-end
+ModLoader.SetupFileHook("lua/Badges_Shared.lua", "lua/CNBadgesFix/Badges_Shared.lua", "replace")
