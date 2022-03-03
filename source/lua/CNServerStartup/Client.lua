7d88c2c4da8f11ecafb63a1454113215b01a3fac
striter <letsbattle@foxmail.com>
2022-03-04 01:38:02 +0800

    Update

---
+++ source/lua/CNServerStartup/Client.lua
@@ -0,0 +1,12 @@
+if kTranslateMessage then
+    kTranslateMessage["SWITCH_SERVER_TO"] = "将服务器切换至<%s>?"
+
+    kTranslateMessage["VOTE_MUTE_PLAYER"] = "禁言玩家"
+    kTranslateMessage["VOTE_MUTE_PLAYER_QUERY"] = "将<%s>禁言半小时?"
+
+    kTranslateMessage["VOTE_FORCE_SPECTATE"] = "强制观战"
+    kTranslateMessage["VOTE_FORCE_SPECTATE_QUERY"] = "将<%s>强制移到观战?"
+
+    kTranslateMessage["VOTE_KILL_PLAYER"] = "击杀玩家"
+    kTranslateMessage["VOTE_KILL_PLAYER_QUERY"] = "将<%s>强制击杀?"
+end
--- source/lua/CNServerStartup/Voting.lua
+++ source/lua/CNServerStartup/Voting.lua
@@ -1,16 +1,92 @@
-    RegisterVoteType( "VoteSwitchServer", { ip = "string (25)", name = "string(20)" } )
+--Server Switch
+RegisterVoteType( "VoteSwitchServer", { ip = "string (25)", name = "string(20)" } )
+RegisterVoteType("VoteMutePlayer", { targetClient = "integer" })
+RegisterVoteType("VoteForceSpectator", { targetClient = "integer" })
+RegisterVoteType("VoteKillPlayer", { targetClient = "integer" })
     
-    if Server then
-		SetVoteSuccessfulCallback( "VoteSwitchServer", 3, function( msg )
-            -- Shared.Message(msg.name .. " " .. msg.ip)
-            Server.SendNetworkMessage("Redirect",{ ip = msg.ip }, true)
-		end )
+if Client then
+    local function GetPlayerList()
+
+        local playerList = Scoreboard_GetPlayerList()
+        local menuItems = { }
+        for p = 1, #playerList do
+
+            local name = Scoreboard_GetPlayerData(Client.GetLocalClientIndex(), "Name")
+            local steamId = Scoreboard_GetPlayerRecord(playerList[p].client_index).SteamId
+            if  steamId ~= 0 and playerList[p].name ~= name then
+                table.insert(menuItems, { text = playerList[p].name, extraData = { targetClient = playerList[p].client_index } })
+            end
+
+        end
+        return menuItems
+
     end
 
-    if Client then
-        AddVoteSetupCallback( function( VoteMenu )
-            AddVoteStartListener( "VoteSwitchServer", function( msg )
-                return "切换服务器至<" .. msg.name ..">?"
-            end )
+    local function SetupAdditionalVotes(voteMenu)
+
+        AddVoteStartListener( "VoteSwitchServer", function( msg )
+            return string.format("SWITCH_SERVER_TO",msg.name)
         end )
+
+        voteMenu:AddMainMenuOption(Locale.ResolveString("VOTE_MUTE_PLAYER"), GetPlayerList, function( msg )
+            AttemptToStartVote("VoteMutePlayer", { targetClient = msg.targetClient })
+        end)
+        
+        AddVoteStartListener("VoteMutePlayer", function(msg)
+            return string.format(Locale.ResolveString("VOTE_MUTE_PLAYER_QUERY"), Scoreboard_GetPlayerName(msg.targetClient))
+        end)
+
+        voteMenu:AddMainMenuOption(Locale.ResolveString("VOTE_FORCE_SPECTATE"), GetPlayerList, function( msg )
+            AttemptToStartVote("VoteForceSpectator", { targetClient = msg.targetClient })
+        end)
+
+        AddVoteStartListener("VoteForceSpectator", function(msg)
+            return string.format(Locale.ResolveString("VOTE_FORCE_SPECTATE_QUERY"), Scoreboard_GetPlayerName(msg.targetClient))
+        end)
+
+        
+        voteMenu:AddMainMenuOption(Locale.ResolveString("VOTE_KILL_PLAYER"), GetPlayerList, function( msg )
+            AttemptToStartVote("VoteKillPlayer", { targetClient = msg.targetClient })
+        end)
+
+        AddVoteStartListener("VoteKillPlayer", function(msg)
+            return string.format(Locale.ResolveString("VOTE_KILL_PLAYER_QUERY"), Scoreboard_GetPlayerName(msg.targetClient))
+        end)
     end
+    AddVoteSetupCallback(SetupAdditionalVotes)
+    
+end
+
+if Server then
+    SetVoteSuccessfulCallback( "VoteSwitchServer", 3, function( msg )
+        -- Shared.Message(msg.name .. " " .. msg.ip)
+        Server.SendNetworkMessage("Redirect",{ ip = msg.ip }, true)
+    end )
+
+    SetVoteSuccessfulCallback("VoteMutePlayer", 3, function( msg )
+        local client = Server.GetClientById(msg.targetClient)
+        if client then
+            Shared.ConsoleCommand(string.format("sh_gag %s %s", client:GetUserId(), 30 * 60))
+        end
+    end)
+
+    SetVoteSuccessfulCallback("VoteForceSpectator", 3, function( msg )
+        local client = Server.GetClientById(msg.targetClient)
+        if client then
+			local Player = client:GetControllingPlayer()
+			if Player then
+                GetGamerules():JoinTeam( Player, kSpectatorIndex, true, true )
+            end
+        end
+    end)
+
+    SetVoteSuccessfulCallback("VoteKillPlayer", 3, function( msg )
+        local client = Server.GetClientById(msg.targetClient)
+        if client then
+			local Player = client:GetControllingPlayer()
+			if Player then
+				Player:Kill( nil, nil, Player:GetOrigin() )
+			end
+        end
+    end)
+end
